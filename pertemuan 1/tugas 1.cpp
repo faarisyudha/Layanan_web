5a526587621281bdda6d84a69076a81e38b0c23e
faarisyudha <147029859+faarisyudha@users.noreply.github.com>
2023-10-12 14:36:35 +0700

    Add files via upload

---
+++ pertemuan 1/tugas 1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int jari;
+    int luas; jari= 7.5;
+
+    luas= 1.3 * 3.14 * jari * jari * jari ;
+
+
+
+    printf("Luas volume adalah %d", luas);
+
+    return 0;
+
+}
